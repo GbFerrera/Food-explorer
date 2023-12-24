a0fab59da75e957299fe9bd79424b24dac2717d7
GbFerrera <124904382+GbFerrera@users.noreply.github.com>
2023-12-24 11:37:16 -0300

    configurando deploy

--- Food-Explorer/vite.config.js
+++ Food-Explorer/vite.config.js
@@ -4,16 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-  build: {
-    target: 'es2015',
-    outDir: 'dist', 
-    assetsDir: 'assets', 
-    sourcemap: true, 
-    minify: 'terser', 
-    assetsInclude: ['/**/*.js'],
-  },
-  server: {
-    port: 3000, 
-  },
+  base: "/Food_explorer-Front"
 });
