4a78a0f19910f59bcc21842b72f8499419e8fbbc
GbFerrera <124904382+GbFerrera@users.noreply.github.com>
2023-12-24 10:14:54 -0300

    mudando nas configs do vite

--- Food-Explorer/vite.config.js
+++ Food-Explorer/vite.config.js
@@ -4,14 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Este é o diretório base em que seu aplicativo será hospedado
+  base: './', 
   build: {
-    outDir: 'dist', // Este é o diretório de saída dos arquivos de build
-    assetsDir: 'assets', // Diretório onde os assets (como imagens) serão colocados dentro de outDir
-    sourcemap: true, // Gere sourcemaps para facilitar a depuração
-    minify: 'terser', // Minificar os arquivos de build usando o Terser
+    target: 'es2015',
+    outDir: 'dist', 
+    assetsDir: 'assets', 
+    sourcemap: true, 
+    minify: 'terser', 
+    assetsInclude: ['/**/*.js'],
   },
   server: {
-    port: 3000, // Porta em que o servidor Vite irá rodar localmente
+    port: 3000, 
   },
 });
