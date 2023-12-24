367ce8fed317d9828d101f348466a622a51b49c0
GbFerrera <124904382+GbFerrera@users.noreply.github.com>
2023-12-24 08:16:44 -0300

    tentativa de resolução de erro do deploy

--- Food-Explorer/vite.config.js
+++ Food-Explorer/vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './', // Este é o diretório base em que seu aplicativo será hospedado
   build: {
-    outDir: 'dist', 
+    outDir: 'dist', // Este é o diretório de saída dos arquivos de build
+    assetsDir: 'assets', // Diretório onde os assets (como imagens) serão colocados dentro de outDir
+    sourcemap: true, // Gere sourcemaps para facilitar a depuração
+    minify: 'terser', // Minificar os arquivos de build usando o Terser
   },
-})
+  server: {
+    port: 3000, // Porta em que o servidor Vite irá rodar localmente
+  },
+});
