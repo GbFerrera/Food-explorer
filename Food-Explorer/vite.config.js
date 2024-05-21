c9b3486f3043bc0aa0b5990d4178df994513d7e7
GbFerrera <124904382+GbFerrera@users.noreply.github.com>
2024-05-21 10:29:56 -0300

    Teste

--- Food-Explorer/vite.config.js
+++ Food-Explorer/vite.config.js
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Food_explorer-Front"
+  base: "/Food_explorer-Front/",
+  server: {
+    fs: {
+      allow: [
+        '/Users/gabrielferreira/node_modules/slick-carousel/slick/fonts',
+        '/Users/gabrielferreira/Desktop/TCC-FRONT/Food-Explorer'
+      ]
+    }
+  },
+  build: {
+    outDir: 'dist', // opcional: especifica o diretório de saída para a build de produção
+  }
 });
+  
